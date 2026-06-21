1095a5691d0a4c9bf2603dbfcaa2b9055a740e3d
= <fweg8256@gmail.com>
2026-06-22 04:52:55 +0900

    feat: add type_check utility to dev_util

---
+++ utils/dev_utils.py
@@ -0,0 +1,11 @@
+import torch
+from torch import Tensor
+from typing import Any
+
+def type_check(*values:tuple[str, Any, type], func_name, strict=True):
+    for value in values:
+        if not isinstance(value[1], value[2]):
+            if strict:
+                raise TypeError(f"<{func_name}> {value[0]}의 타입이 부적절합니다. 예상한 타입:{value[2]}")
+            return False
+    return True
