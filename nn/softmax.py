8727b450e68b92365a91f39af31efabadb1183be
GradAccumulator <fweg8256@gmail.com>
2026-06-23 03:03:21 +0900

    폴더 이름 models->nn으로 변경, softmax 추가

---
+++ nn/softmax.py
@@ -0,0 +1,27 @@
+import torch, torch.nn as nn
+from torch import Tensor
+from ..utils import dev_utils
+
+class Softmax(nn.Module):
+    def __init__(self, dim:int=-1, temperature:float=1.0):
+        super().__init__()
+        dev_utils.type_check(
+            ("dim", dim, int),
+            ("temperature", temperature, float|int)
+        )
+        self._dim = dim
+        self._temperature = temperature
+        
+        if self.temperature<=0:
+            raise ValueError("Softmax의 temperature은 양수여야 합니다.")
+    
+    def forward(self, x:Tensor) -> Tensor:
+        x = x / self.temperature
+        x = x - x.max(dim=self.dim, keepdim=True).values
+        exp_x = x.exp()
+        return exp_x/exp_x.sum(dim=self.dim, keepdim=True)
+    
+    @property
+    def dim(self): return self._dim
+    @property
+    def temperature(self): return self._temperature
