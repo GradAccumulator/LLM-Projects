c7b425e9b02042af046c1194e7da1f6b84195e9c
JuHyoungLee <fweg8256@gmail.com>
2026-07-16 08:12:43 +0900

    fix: 토크나이저 폴더 구조 수정, 모델 config 파일 이름 수정

--- tokenizer/train_tokenizer.py
+++ tokenizer/train_tokenizer.py
@@ -1,20 +1,18 @@
 import sentencepiece as spm
 
+model_name = "32k_fineweb2_korean"
 spm.SentencePieceTrainer.train(
     input=r"C:\Users\LEEJUHYOUNG\Documents\LLM Projects\datasets\llm\fineweb2_korean\processed\train.txt",
-    model_prefix="tokenizer/32k_fineweb2_korean",
+    model_prefix=f"tokenizer/{model_name}/{model_name}",
     vocab_size=32000,
     model_type="bpe",
-    input_sentence_size=10_000_000,
+    input_sentence_size=20_000_000,
     shuffle_input_sentence=True,
 
     pad_id=0,
-    bos_id=1,
-    eos_id=2,
-    unk_id=3,
+    unk_id=2,
+    bos_id=-1,
+    eos_id=-1,
 
-    pad_piece="<pad>",
-    bos_piece="<bos>",
-    eos_piece="<eos>",
-    unk_piece="<unk>",
+    user_defined_symbols=["<eos>"],
 )
